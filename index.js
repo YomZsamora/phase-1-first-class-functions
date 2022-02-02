ba7f15211c8bcd7892db960c1da13b10b0ff1305
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 18:11:47 +0300

    Create function that returns a named function.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Receives a callback function and calls it
 function receivesAFunction(callbackFunction) { callbackFunction(); }
+
+function returnsANamedFunction() { 
+	return function namedFunction() {} 
+}
